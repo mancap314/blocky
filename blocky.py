9b3c003d0a18b7212f174955670881ead88b3b96
mancap314 <egregius314@gmail.com>
2018-01-06 16:10:45 +0100

    function build_block added (still to be tested and documented)

--- blocky.py
+++ blocky.py
@@ -5,6 +5,7 @@ from Crypto.Hash import SHA256
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 import six
+import datetime
 import base64
 
 home_directory = os.path.expanduser('~')
@@ -43,10 +44,15 @@ def to_format_for_encrypt(value):
     if isinstance(value, six.binary_type):
         return value
 
-def encrypt(message, recipient):
-    publicKeyPathRecipient = os.path.join(foreignPublicKeysDirectory, '{}.key'.format(recipient))
-    with open(publicKeyPathRecipient) as f:
-        key_text = f.read()
+def encrypt(message, recipient, auto=False):
+    if auto:
+        with open(publicKeyPath) as f:
+            key_text = f.read()
+    else:
+        publicKeyPathRecipient = os.path.join(foreignPublicKeysDirectory, '{}.key'.format(recipient))
+        with open(publicKeyPathRecipient) as f:
+            key_text = f.read()
+
     public_key_object = RSA.importKey(key_text)
     cipher = PKCS1_OAEP.new(public_key_object)
     message = to_format_for_encrypt(message)
@@ -68,7 +74,7 @@ def sign(text):
         key_text = f.read()
     private_key_object = RSA.importKey(key_text)
     text = text.encode('utf8')
-    hash = SHA256.new(text).digest()
+    hash = SHA256.new(text).hexdigest()
     signature = private_key_object.sign(hash, '')
     return signature
 
@@ -78,11 +84,47 @@ def verify_signature(text, signature, author):
     with open(authorPublicKeyPath) as f:
         key_text = f.read()
     authorPublicKey = RSA.importKey(key_text)
-    hash = SHA256.new(text).digest()
+    hash = SHA256.new(text).hexdigest()
     return authorPublicKey.verify(hash, signature)
 
 
-# TEST
-# setup() #OK
-# create_self_keys() #OK
-# print(sign('truc'))
+def build_block(previous_block_hash, message, recipients, difficulty=0):
+    blockstring = previous_block_hash
+    timestamp = datetime.datetime.utcfromtimestamp(datetime.datetime.now().timestamp()) #current utc time
+    timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    blockstring += timestamp
+    message_hash = SHA256.new(message).hexdigest()
+    blockstring += message_hash
+    message_signature = sign(message_hash)
+    blockstring += message_signature
+
+    with open(publicKeyPath) as f:
+        key_text = f.read()
+    author = SHA256.new(key_text).hexdigest()
+    blockstring += str(author)
+
+    message_encryptions = []
+    auto_encryption = str(encrypt(message, auto=True))
+    blockstring += auto_encryption
+    message_encryptions.append({'recipent': 'auto', 'encrypted': auto_encryption})
+    recipients = recipients.sort()
+    for recipient in recipients:
+        encrypted_message = str(encrypt(message, recipient))
+        recipient = str(recipient)
+        message_encryptions.append({'recipient': recipient, 'encrypted': encrypted_message})
+        blockstring += recipient + encrypted_message
+
+    block_hash = str(SHA256.new(key_text).hexdigest())
+    if difficulty > 0:
+        nonce = 0
+        while not block_hash.startswith('0' * difficulty):
+            block_hash = str(SHA256.new(blockstring).hexdigest() + str(nonce))
+            nonce += 1
+
+    return {'previous_block_has': previous_block_hash, 'timestamp': timestamp, 'author': author,
+            'message_hash': message_hash, 'encryptions': message_encryptions,  'difficulty': str(difficulty),
+            'nonce': nonce, 'block_hash': block_hash}
+
+
+
+
