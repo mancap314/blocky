5f90d562ffe892d0cb033fdd257bb5b4c2380fc1
mancap314 <egregius314@gmail.com>
2018-01-07 17:37:04 +0100

    build_block fixed and tested + block content verification implemented and tested

--- blocky.py
+++ blocky.py
@@ -6,6 +6,8 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 import six
 import datetime
+from operator import itemgetter
+from pytz import timezone
 import base64
 
 home_directory = os.path.expanduser('~')
@@ -44,7 +46,7 @@ def to_format_for_encrypt(value):
     if isinstance(value, six.binary_type):
         return value
 
-def encrypt(message, recipient, auto=False):
+def encrypt(message, recipient=None, auto=False):
     if auto:
         with open(publicKeyPath) as f:
             key_text = f.read()
@@ -74,7 +76,7 @@ def sign(text):
         key_text = f.read()
     private_key_object = RSA.importKey(key_text)
     text = text.encode('utf8')
-    hash = SHA256.new(text).hexdigest()
+    hash = SHA256.new(text).digest()
     signature = private_key_object.sign(hash, '')
     return signature
 
@@ -84,46 +86,97 @@ def verify_signature(text, signature, author):
     with open(authorPublicKeyPath) as f:
         key_text = f.read()
     authorPublicKey = RSA.importKey(key_text)
-    hash = SHA256.new(text).hexdigest()
+    hash = SHA256.new(text).digest()
     return authorPublicKey.verify(hash, signature)
 
 
 def build_block(previous_block_hash, message, recipients, difficulty=0):
-    blockstring = previous_block_hash
+    blockstring = str(previous_block_hash)
     timestamp = datetime.datetime.utcfromtimestamp(datetime.datetime.now().timestamp()) #current utc time
-    timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = timestamp.replace(tzinfo=timezone('UTC'))
+    timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S %Z')
     blockstring += timestamp
-    message_hash = SHA256.new(message).hexdigest()
-    blockstring += message_hash
+    message_hash = SHA256.new(message.encode('utf8')).hexdigest()
+    blockstring += str(message_hash)
     message_signature = sign(message_hash)
-    blockstring += message_signature
+    blockstring += str(message_signature)
+    blockstring += str(difficulty)
 
     with open(publicKeyPath) as f:
         key_text = f.read()
-    author = SHA256.new(key_text).hexdigest()
+    author = SHA256.new(key_text.encode('utf8')).hexdigest()
     blockstring += str(author)
 
     message_encryptions = []
     auto_encryption = str(encrypt(message, auto=True))
-    blockstring += auto_encryption
-    message_encryptions.append({'recipent': 'auto', 'encrypted': auto_encryption})
-    recipients = recipients.sort()
+    recipients.append(author)
+    recipients.sort()
     for recipient in recipients:
-        encrypted_message = str(encrypt(message, recipient))
-        recipient = str(recipient)
-        message_encryptions.append({'recipient': recipient, 'encrypted': encrypted_message})
-        blockstring += recipient + encrypted_message
+        if recipient == author:
+            message_encryptions.append({'recipient': recipient, 'encrypted': auto_encryption})
+            blockstring += str(recipient) + auto_encryption
+        else:
+            encrypted_message = str(encrypt(message, recipient))
+            recipient = str(recipient)
+            message_encryptions.append({'recipient': recipient, 'encrypted': encrypted_message})
+            blockstring += recipient + encrypted_message
 
-    block_hash = str(SHA256.new(key_text).hexdigest())
+    block_hash = str(SHA256.new(blockstring.encode('utf8')).hexdigest())
+    nonce = ''
     if difficulty > 0:
-        nonce = 0
+        nonce = -1
         while not block_hash.startswith('0' * difficulty):
-            block_hash = str(SHA256.new(blockstring).hexdigest() + str(nonce))
             nonce += 1
+            block_hash = str(SHA256.new((blockstring + str(nonce)).strip().encode('utf8')).hexdigest())
 
-    return {'previous_block_has': previous_block_hash, 'timestamp': timestamp, 'author': author,
+
+    print('blockstring: {}'.format(blockstring + str(nonce)))
+
+    return {'previous_block_hash': previous_block_hash, 'timestamp': timestamp, 'author': author,
             'message_hash': message_hash, 'encryptions': message_encryptions,  'difficulty': str(difficulty),
-            'nonce': nonce, 'block_hash': block_hash}
+            'nonce': nonce, 'block_hash': block_hash, 'signature': message_signature}
+
+
+def verify_block_content(block):
+    # verify signature
+    signature_ok = verify_signature(block['message_hash'], block['signature'], block['author'])
+    if not signature_ok:
+        print('WARNING: block signature not correct')
+
+    # verify difficulty
+    difficulty_ok = True
+    difficulty = int(block['difficulty'])
+    if difficulty > 0:
+        difficulty_ok = block['block_hash'].startswith('0' * difficulty)
+    if not difficulty_ok:
+        print('WARNING: block hash does not respect block difficulty')
+
+    # verify block hash
+    blockstring = str(block['previous_block_hash']) + str(block['timestamp']) + str(block['message_hash']) + str(block['signature']) + str(block['difficulty']) + str(block['author'])
+    encryptions = block['encryptions']
+    encryptions = sorted(encryptions, key=itemgetter('recipient'))
+    for encryption in encryptions:
+        blockstring += str(encryption['recipient']) + str(encryption['encrypted'])
+    blockstring += str(block['nonce'])
+    print('blockstring: {}'.format(blockstring))
+    block_hash = str(SHA256.new(str(blockstring).strip().encode('utf8')).hexdigest())
+    print('block_hash computed: {}\nblock_hash gotten: {}'.format(block_hash, block['block_hash']))
+    block_hash_ok = (block_hash == block['block_hash'])
+    if not block_hash_ok:
+        print('WARNING: block_hash is not valid for this block')
+
+    #verify that the timestamp doesn't lie in the future
+    tblock_timestamp = datetime.datetime.strptime(block['timestamp'], '%Y-%m-%d %H:%M:%S %Z')
+    now = datetime.datetime.utcfromtimestamp(datetime.datetime.now().timestamp())
+    timestamp_ok = (tblock_timestamp < now)
+    if not timestamp_ok:
+        print('WARNING: Block timestamp lies in the future')
+
+    return signature_ok & difficulty_ok & block_hash_ok & timestamp_ok
+
+
+
+
 
 
 
